921be3f055a8cf9abe965fc118c871b99d825c9b
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-01 15:00:41 +0300

    Initial commit

---
+++ src/main/java/org/example/InitialMortgagePayment.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class InitialMortgagePayment {
+    public static void main(String[] args) {
+        long apartmentCost = 7000000L;
+        double initialPaymentPercent = 0.10;
+        double initialPaymentAmount = apartmentCost * initialPaymentPercent;
+    }
+}
